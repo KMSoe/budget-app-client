b95e22d00fa48898097dcc6dcd9edff7d1e2db84
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-03-15 21:28:10 +0630

    Load Categories Data and show roughly

---
+++ src/components/categories/Categories.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <Breadcrumb
+      :route="{ name: 'categories' }"
+      :is-active="true"
+      icon-class="fas fa-th-large"
+      currentPage="Categories"
+    ></Breadcrumb>
+
+    <section class="categories-section justify-content-center py-4">
+      <h3 class="text-center">Categories</h3>
+      <div class="col-md-9 col-lg-12 px-3 px-lg-5">
+        <div class="swip-nav d-lg-none d-flex justify-content-center my-3">
+          <a class="income-nav active btn select-btn text-white me-3 py-1 px-3">
+            <i class="fas fa-check me-2"></i>Income
+          </a>
+          <a class="expense-nav btn select-btn text-white me-3 py-1 px-3">
+            <i class="fas fa-check me-2"></i>Expense
+          </a>
+        </div>
+        <div class="row mt-5">
+            <category-list :categoryData="incomeCategoryData"></category-list>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+import CategoryList from "./CategoryList.vue";
+
+export default {
+  components: {
+    "category-list": CategoryList,
+  },
+  computed: {
+    ...mapGetters(["incomeCategories", "expenseCategories"]),
+    incomeCategoryData() {
+      return { type: "Income", data: this.incomeCategories };
+    },
+    expenseCategoryData() {
+      return { type: "Expense", data: this.expenseCategories };
+    },
+  },
+};
+</script>
+<style>
+</style>
---
+++ src/components/categories/CategoryList.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="col-lg-6 categories-container">
+    <h5 class="mb-3 ps-5">{{ categoryData.type }} Categories</h5>
+    <ul class="categories pe-4" v-if="categoryData.data.length">
+      <li class="d-flex py-2" v-for="cat in categoryData.data" :key="cat.id">
+        <i
+          :class="`cat-icon ${cat.class} me-3 my-auto`"
+          :style="{
+            backgroundColor: cat.color,
+          }"
+        ></i>
+        <span class="flex-fill my-auto pe-4 pe-lg-0 text-center">{{
+          cat.name
+        }}</span>
+
+        <form class="mt-2" style="box-sizing: border-box">
+          <button
+            type="submit"
+            class="text-white my-auto"
+            style="outline: none; border: none; background-color: unset"
+          >
+            <i class="fas fa-times-circle"></i>
+          </button>
+        </form>
+      </li>
+    </ul>
+    <p class="text-center text-muted mt-5" v-else>No category</p>
+
+    <a href="#" class="my-3 ms-4 btn bg-plus text-white">
+      <i class="fas fa-plus"></i>
+      <span class="">Add Income Category</span>
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    categoryData: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+<style scoped>
+ul.categories li {
+  color: var(--text-color);
+  font-size: 18px;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-bottom: 1px solid #3f3f3f;
+  transition: background-color 0.3s ease;
+}
+ul.categories li:last-of-type {
+  border: 0;
+}
+ul.categories li:hover {
+  background-color: #3f3f3f;
+}
+i.cat-icon {
+  color: var(--text-color);
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  position: relative;
+}
+i.cat-icon::before {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
